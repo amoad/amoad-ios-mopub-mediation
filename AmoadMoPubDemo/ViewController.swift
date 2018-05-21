47e65fa3c57eaacc121e11fa427e1fad0e318bc5
菅原 清吾 <sugawara_seigo@cyberagent.co.jp>
2018-05-21 13:18:02 +0900

    adding afioVC

--- AmoadMoPubDemo/ViewController.swift
+++ AmoadMoPubDemo/ViewController.swift
@@ -22,7 +22,7 @@ fileprivate struct Const {
     public static let items: [Item] = [
         Item(title: "バナー広告", storyboardName: "Banner"),
         Item(title: "インフィード広告", storyboardName: "Infeed"),
-        Item(title: "インフィード AfiO 広告", storyboardName: "InfeedAfio")
+        Item(title: "インフィード AfiO 広告", storyboardName: "Afio")
     ]
 }
 
