9c875c6e3e34b4f3e1fb9c894e70f6eab1dacbdb
菅原 清吾 <sugawara_seigo@cyberagent.co.jp>
2018-05-25 20:50:27 +0900

    fixing infeedAfio

--- AmoadMoPubDemo/MoPubAdapter/AmoadMoPubAdapterInfeedAfio.swift
+++ AmoadMoPubDemo/MoPubAdapter/AmoadMoPubAdapterInfeedAfio.swift
@@ -20,23 +20,29 @@ class AmoadMoPubAdapterInfeedAfio: MPBannerCustomEvent, AMoAdNativeAppDelegate {
         }
         
         // Sending adData to amoadSDK
-        self.preparedInfeedAfio(customEventClassData: _customEventClassData)
+        self.preparedInfeedAfio(size:size, customEventClassData: _customEventClassData)
     }
     
-    fileprivate func preparedInfeedAfio(customEventClassData: AmoadCustomEventClassDataForInfeedAfio) {
+    fileprivate func preparedInfeedAfio(size: CGSize, customEventClassData: AmoadCustomEventClassDataForInfeedAfio) {
         
         AMoAdNativeViewManager.shared().setEnvStaging(true)
-        
+
         // 広告 View を xib から生成する
         let view = Bundle.main.loadNibNamed(customEventClassData.file, owner: nil, options: nil)?.first as! UIView
+        view.translatesAutoresizingMaskIntoConstraints = false
         
+        let adView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: size.width, height: size.height))
+        adView.addSubview(view)
+        adView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-(1)-[view]-(1)-|", options:.alignAllCenterX, metrics: nil, views: ["view": view]))
+        adView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-(1)-[view]-(1)-|", options:.alignAllCenterY, metrics: nil, views: ["view": view]))
+
         // 広告準備
         AMoAdNativeViewManager.shared().prepareAd(withSid: customEventClassData.sid, iconPreloading: true, imagePreloading: true)
         
         // 広告取得
         AMoAdNativeViewManager.shared().renderAd(withSid: customEventClassData.sid, tag: "", view: view, delegate: self)
 
-        self.delegate.bannerCustomEvent(self, didLoadAd: view)
+        self.delegate.bannerCustomEvent(self, didLoadAd: adView)
     }
     
     func amoadNativeImageDidReceive(_ sid: String!, tag: String!, view: UIView!, state: AMoAdResult) {
