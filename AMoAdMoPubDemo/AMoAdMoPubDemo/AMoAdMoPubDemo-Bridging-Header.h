576b4b7b503c729771a428908292b1638081e260
菅原 清吾 <sugawara_seigo@cyberagent.co.jp>
2018-05-31 20:20:25 +0900

    adding amoadSDK

---
+++ AMoAdMoPubDemo/AMoAdMoPubDemo/AMoAdMoPubDemo-Bridging-Header.h
@@ -0,0 +1,29 @@
+//
+//  AMoAdMoPubDemo-Bridging-Header.h
+//  AMoAdMoPubDemo
+//
+//  Created by 菅原 清吾 on 2018/05/31.
+//  Copyright © 2018年 AMoAd. All rights reserved.
+//
+
+#ifndef AMoAdMoPubDemo_Bridging_Header_h
+#define AMoAdMoPubDemo_Bridging_Header_h
+
+
+#endif /* AMoAdMoPubDemo_Bridging_Header_h */
+
+@import MoPub;
+
+#import "AMoAd.h"
+#import "AMoAdAnalytics.h"
+#import "AMoAdInfeed.h"
+#import "AMoAdInterstitial.h"
+#import "AMoAdInterstitialVideo.h"
+#import "AMoAdItem.h"
+#import "AMoAdList.h"
+#import "AMoAdLogger.h"
+#import "AMoAdNativeMainVideoView.h"
+#import "AMoAdNativePreRoll.h"
+#import "AMoAdNativeView.h"
+#import "AMoAdView.h"
+#import "AMoAdWeb.h"
