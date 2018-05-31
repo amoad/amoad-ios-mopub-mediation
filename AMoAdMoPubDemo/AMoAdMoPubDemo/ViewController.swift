0b9e9f40d072123bba681ce02f3affbc2a134a64
菅原 清吾 <sugawara_seigo@cyberagent.co.jp>
2018-05-31 19:43:38 +0900

    first commit

---
+++ AMoAdMoPubDemo/AMoAdMoPubDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AMoAdMoPubDemo
+//
+//  Created by 菅原 清吾 on 2018/05/31.
+//  Copyright © 2018年 AMoAd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
